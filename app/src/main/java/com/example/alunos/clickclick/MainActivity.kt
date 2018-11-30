93677b0e788d97f518f050fd366043c35a9c8757
alexist98 <alexistoigo@gmail.com>
2018-11-30 20:14:50 -0200

    Adicionando OnSave

--- app/src/main/java/com/example/alunos/clickclick/MainActivity.kt
+++ app/src/main/java/com/example/alunos/clickclick/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.alunos.clickclick
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -17,11 +19,20 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var countDownTimer: CountDownTimer
     internal val initialCountDown: Long = 10000
     internal val countDownInterval: Long = 1000
+    internal  val  TAG = MainActivity::class.java.simpleName
+      var timeLeftOnTimer: Long = 10000
+
+    companion object {
+        private val SCORE_KEY = "SCORE_KEY"
+        private val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate called. Score is: $score")
+
         tapMeButton = findViewById<Button>(R.id.tap_me_button)
         gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
         timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
@@ -32,6 +43,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY,score)
+        outState.putLong(TIME_LEFT_KEY, timeLeftOnTimer)
+        countDownTimer.cancel()
+        Log.d(TAG, "onSaveInstanceState: Saving Score: $score & Time Left: $timeLeftOnTimer")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy called.")
+    }
+
     private fun resetGame(){
         score = 0
         gameScoreTextView.text = getString(R.string.sua_pontuacao, score.toString())
@@ -40,6 +66,7 @@ class MainActivity : AppCompatActivity() {
 
         countDownTimer = object: CountDownTimer(initialCountDown, countDownInterval){
             override fun onTick(millisUntilFinished: Long){
+                timeLeftOnTimer=millisUntilFinished
                 val timeLeft = millisUntilFinished / 1000
                 timeLeftTextView.text = getString(R.string.time_left, timeLeft.toString())
             }
